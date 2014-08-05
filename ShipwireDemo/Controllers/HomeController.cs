3e54c370fe27b2f9861f44c8d174bfe571815ffe
Xudong Wang <OverNothingbupt@gmail.com>
2014-08-05 16:26:38 -0700

    Second Commit. Users can input username and password.

--- ShipwireDemo/Controllers/HomeController.cs
+++ ShipwireDemo/Controllers/HomeController.cs
@@ -10,130 +10,153 @@ using System.Xml.Linq;
 using System.Net;
 using System.IO;
 
-namespace MvcDemo.Controllers
+namespace Shipwire.Controllers
 {
-    public class HomeController : Controller
+    public class ShipwireClient
     {
-        public ActionResult Index()
+        private string requestXml;
+        private string destinationUrl = "https://api.shipwire.com/exec/InventoryServices.php";
+        private XDocument xDoc; //record response in the type of XDocument
+
+        public ShipwireClient(string username, string password, string server, string warehouse, string productcode)
         {
-            //You need to replace Username and Password with your own account in requestXml
-            string requestXml = "<InventoryUpdate><Username>api_user@example.com</Username><Password>yourpassword</Password><Server>Production</Server><Warehouse></Warehouse><ProductCode></ProductCode><IncludeEmpty/></InventoryUpdate>";
-            string destinationUrl = "https://api.shipwire.com/exec/InventoryServices.php";
-
+            string inventoryRequest = "<InventoryUpdate><Username>" + username + "</Username><Password>" + password + "</Password><Server>" + server + "</Server><Warehouse>" + warehouse + "</Warehouse><ProductCode>" + productcode + "</ProductCode><IncludeEmpty/></InventoryUpdate>";
+            requestXml = inventoryRequest;
+        }
+        //Get the response about the Inventory information
+        public string GetInventory()
+        {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(destinationUrl);
-
+            string results = "ConnectError"; //used to notice the Error
             byte[] bytes;
             bytes = System.Text.Encoding.ASCII.GetBytes(requestXml);
-
             request.ContentType = "text/xml; encoding='utf-8'";
             request.ContentLength = bytes.Length;
-
             request.Method = "POST";
             Stream requestStream = request.GetRequestStream();
             requestStream.Write(bytes, 0, bytes.Length);
             requestStream.Close();
             HttpWebResponse response;
             response = (HttpWebResponse)request.GetResponse();
-
             if (response.StatusCode == HttpStatusCode.OK)
             {
                 Stream responseStream = response.GetResponseStream();
                 XmlReader reader = new XmlTextReader(responseStream);
-
-                XDocument xDoc = XDocument.Load(reader);
-                string s = xDoc.ToString();
-                XDocument xDoc2 = XDocument.Parse(s);
-                string TotalProducts = xDoc.Element("InventoryUpdateResponse").Element("TotalProducts").Value;
-                int totalproducts = Convert.ToInt32(TotalProducts);
-
-                //18 Attributes in Shipwire Inventory Response. And convert attributes to array which is easier to use
-                //Product Code
-                var CodeValue = from x in xDoc2.Descendants("Product") select x.Attribute("code").Value;
-                string[] CodeArray = CodeValue.ToArray();
-                //Quantity
-                var QuantityValue = from x in xDoc2.Descendants("Product") select x.Attribute("quantity").Value;
-                string[] QuantityArray = QuantityValue.ToArray();
-                //Pending
-                var PendingValue = from x in xDoc2.Descendants("Product") select x.Attribute("pending").Value;
-                string[] PendingArray = PendingValue.ToArray();
-                //Good
-                var GoodValue = from x in xDoc2.Descendants("Product") select x.Attribute("good").Value;
-                string[] GoodArray = GoodValue.ToArray();
-                //Backordered
-                var BackorderedValue = from x in xDoc2.Descendants("Product") select x.Attribute("backordered").Value;
-                string[] BackorderedArray = BackorderedValue.ToArray();
-                //Reserved
-                var ReservedValue = from x in xDoc2.Descendants("Product") select x.Attribute("reserved").Value;
-                string[] ReservedArray = ReservedValue.ToArray();
-                //Shipping
-                var ShippingValue = from x in xDoc2.Descendants("Product") select x.Attribute("shipping").Value;
-                string[] ShippingArray = ShippingValue.ToArray();
-                //Shipped
-                var ShippedValue = from x in xDoc2.Descendants("Product") select x.Attribute("shipped").Value;
-                string[] ShippedArray = ShippedValue.ToArray();
-                //Consuming
-                var ConsumingValue = from x in xDoc2.Descendants("Product") select x.Attribute("consuming").Value;
-                string[] ConsumingArray = ConsumingValue.ToArray();
-                //Creating
-                var CreatingValue = from x in xDoc2.Descendants("Product") select x.Attribute("creating").Value;
-                string[] CreatingArray = CreatingValue.ToArray();
-                //Consumed
-                var ConsumedValue = from x in xDoc2.Descendants("Product") select x.Attribute("consumed").Value;
-                string[] ConsumedArray = ConsumedValue.ToArray();
-                //Created
-                var CreatedValue = from x in xDoc2.Descendants("Product") select x.Attribute("created").Value;
-                string[] CreatedArray = CreatedValue.ToArray();
-                //ShippedLastDay
-                var ShippedLastDayValue = from x in xDoc2.Descendants("Product") select x.Attribute("shippedLastDay").Value;
-                string[] ShippedLastDayArray = ShippedLastDayValue.ToArray();
-                //ShippedLastWeek
-                var ShippedLastWeekValue = from x in xDoc2.Descendants("Product") select x.Attribute("shippedLastWeek").Value;
-                string[] ShippedLastWeekArray = ShippedLastWeekValue.ToArray();
-                //ShippedLast4Weeks
-                var ShippedLast4WeeksValue = from x in xDoc2.Descendants("Product") select x.Attribute("shippedLast4Weeks").Value;
-                string[] ShippedLast4WeeksArray = ShippedLast4WeeksValue.ToArray();
-                //OrderedLastDay
-                var OrderedLastDayValue = from x in xDoc2.Descendants("Product") select x.Attribute("orderedLastDay").Value;
-                string[] OrderedLastDayArray = OrderedLastDayValue.ToArray();
-                //OrderedLastWeek
-                var OrderedLastWeekValue = from x in xDoc2.Descendants("Product") select x.Attribute("orderedLastWeek").Value;
-                string[] OrderedLastWeekArray = OrderedLastWeekValue.ToArray();
-                //OrderedLast4Weeks
-                var OrderedLast4WeeksValue = from x in xDoc2.Descendants("Product") select x.Attribute("orderedLast4Weeks").Value;
-                string[] OrderedLast4WeeksArray = OrderedLast4WeeksValue.ToArray();
-
+                xDoc = XDocument.Load(reader);
+                results = xDoc.ToString();
                 //Close XMLReader
                 reader.Close();
                 responseStream.Close();
-                //response.Close();
+                response.Close();
 
-                //Pass 18 attributes to ViewBag so that View can make use of 18 attributes
-                ViewBag.Name = xDoc;
-                ViewBag.TotalProducts = totalproducts;
-
-                ViewBag.Code = CodeArray;
-                ViewBag.Quantity = QuantityArray;
-                ViewBag.Pending = PendingArray;
-                ViewBag.Good = GoodArray;
-                ViewBag.Backordered = BackorderedArray;
-                ViewBag.Reserved = ReservedArray;
-                ViewBag.Shipping = ShippingArray;
-                ViewBag.Shipped = ShippedArray;
-                ViewBag.Consuming = ConsumingArray;
-                ViewBag.Creating = CreatingArray;
-                ViewBag.Consumed = ConsumedArray;
-                ViewBag.Created = CreatedArray;
-                ViewBag.ShippedLastDay = ShippedLastDayArray;
-                ViewBag.ShippedLastWeek = ShippedLastWeekArray;
-                ViewBag.ShippedLast4Weeks = ShippedLast4WeeksArray;
-                ViewBag.OrderedLastDay = OrderedLastDayArray;
-                ViewBag.OrderedLastWeek = OrderedLastWeekArray;
-                ViewBag.OrderedLast4Weeks = OrderedLast4WeeksArray;
-
-
+                return results;
             }
             response.Close();
+            return results;
+        }
+        //Check whether the response is correct
+        public string Validate()
+        {
+            string status = xDoc.Element("InventoryUpdateResponse").Element("Status").Value;
+            if (status == "0" || status == "Test")
+            {
+                //If the response is correct, we need to store TotalProducts which will be used later in loop
+                string TotalProducts = xDoc.Element("InventoryUpdateResponse").Element("TotalProducts").Value;
+                return TotalProducts;
+            }
+            else
+            {
+                return "InfoError";
+            }
+        }
+    }
+
+    public class Products
+    {
+        private string product = "Product";
+        private XDocument xDoc;
+        public Products(string results)
+        {
+            xDoc = XDocument.Parse(results);
+        }
+
+        public string[] Parse(string field)
+        {
+            var Value = from x in xDoc.Descendants(product) select x.Attribute(field).Value;
+            string[] Array = Value.ToArray();
+            return Array;
+        }
+    }
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
             return View();
         }
+
+        public ActionResult Inventory()
+        {
+            string username = Request["username"];
+            string password = Request["password"];
+            string server = Request["server"];
+            string warehouse = Request["warehouse"];
+            string productcode = Request["productcode"];
+            ShipwireClient shipwireClient = new ShipwireClient(username, password, server, warehouse, productcode);
+            //Check whether the connection is normal
+            if (shipwireClient.GetInventory() == "ConnectError")
+            {
+                return RedirectToAction("ConnectError");
+            }
+            //Check whether the input is correct
+            if (shipwireClient.Validate() == "InfoError")
+            {
+                return RedirectToAction("InfoError");
+            }
+            else
+            {
+                //If the response is correct, we need to store TotalProducts which will be used later in loop
+                ViewBag.TotalProducts = Convert.ToInt32(shipwireClient.Validate());
+            }
+
+            Products products = new Products(shipwireClient.GetInventory());
+            //18 fields
+            ViewBag.Code = products.Parse("code");
+            ViewBag.Quantity = products.Parse("quantity");
+            ViewBag.Pending = products.Parse("pending");
+            ViewBag.Good = products.Parse("good");
+            ViewBag.Backordered = products.Parse("backordered");
+            ViewBag.Reserved = products.Parse("reserved");
+            ViewBag.Shipping = products.Parse("shipping");
+            ViewBag.Shipped = products.Parse("shipped");
+            ViewBag.Creating = products.Parse("creating");
+            ViewBag.Consuming = products.Parse("consuming");
+            ViewBag.Created = products.Parse("created");
+            ViewBag.Consumed = products.Parse("consumed");
+            ViewBag.ShippedLastDay = products.Parse("shippedLastDay");
+            ViewBag.ShippedLastWeek = products.Parse("shippedLastWeek");
+            ViewBag.ShippedLast4Weeks = products.Parse("shippedLast4Weeks");
+            ViewBag.OrderedLastDay = products.Parse("orderedLastDay");
+            ViewBag.OrderedLastWeek = products.Parse("orderedLastWeek");
+            ViewBag.OrderedLast4Weeks = products.Parse("orderedLast4Weeks");
+
+            return View();
+        }
+
+        public ActionResult InfoError()
+        {
+            ViewBag.Message = "Error with Valid Username/EmailAddress and Password Required. There is an error in XML document.";
+
+            return View();
+        }
+
+        public ActionResult ConnectError()
+        {
+            ViewBag.Message = "Cannot get response from shipwire server successfully.";
+
+            return View();
+        }
+
+
     }
 }
